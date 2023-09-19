d0ec5348bf489d48da9f5cae53b8256329a9dcd1
DungPC <dungnt30101995@gmail.com>
2023-09-19 14:08:35 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Topbar from "./views/global/Topbar";
 import Sidebar from "./views/global/Sidebar";
 import Dashboard from "./views/dashboard";
 import SignInSide from "./views/auth/SignInSide";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-// import Calendar from "./scenes/calendar/calendar";
+import { checkUserToken } from "./ultis";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
-  const token = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  useEffect(() => {
+    const result = checkUserToken();
+    setIsLoggedIn(result);
+    let pathName =
+      (!["/sing-in"].includes(location.pathname) && !result) ? "/sing-in" : "/";
+    navigate(pathName);
+  }, [isLoggedIn]);
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {token ? (
+        {isLoggedIn ? (
           <div className="app">
             <Sidebar isSidebar={isSidebar} />
             <main className="content">
---
+++ src/ultis/index.js
@@ -0,0 +1,6 @@
+const checkUserToken = () => {
+  const userToken = false;
+  if (!userToken || userToken === "undefined")
+    return !userToken || userToken === "undefined" ? false : true;
+};
+export { checkUserToken };
