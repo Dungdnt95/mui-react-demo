1e64407dc358c50f6282cccf5401a89189a7a66c
DungPC <dungnt30101995@gmail.com>
2023-09-19 16:08:33 +0700

    update

--- server/index.js
+++ server/index.js
@@ -6,10 +6,33 @@ import { userRoutes } from "./routes/userRoutes.js";
 import { authRoutes } from "./routes/authRoutes.js";
 
 const app = express();
+var corsOptions = {
+  origin: process.env.ORIGIN,
+};
+const Role = db.role;
 
-db.sequelize.sync({ force: false });
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and Resync Db");
+  initial();
+});
+const initial = () => {
+  Role.create({
+    id: 1,
+    name: "user",
+  });
 
-app.use(cors());
+  Role.create({
+    id: 2,
+    name: "moderator",
+  });
+
+  Role.create({
+    id: 3,
+    name: "admin",
+  });
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
