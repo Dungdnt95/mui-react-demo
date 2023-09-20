5b71a48ad518a6ddd4977ead901bfa1d714a03b4
DungPC <dungnt30101995@gmail.com>
2023-09-20 15:35:40 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,30 +3,16 @@ import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import Topbar from "./views/global/Topbar";
 import Sidebar from "./views/global/Sidebar";
 import Dashboard from "./views/dashboard";
-import SignInSide from "./views/auth/SignInSide";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { checkUserToken } from "./ultis";
 
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
-  const location = useLocation();
-  // useEffect(() => {
-  //   const result = checkUserToken();
-  //   setIsLoggedIn(result);
-  //   let pathName =
-  //     (!["/sing-in"].includes(location.pathname) && !result) ? "/sing-in" : "/";
-  //   navigate(pathName);
-  // }, [isLoggedIn]);
-
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {/* {!isLoggedIn ? ( */}
           <div className="app">
             <Sidebar isSidebar={isSidebar} />
             <main className="content">
@@ -36,11 +22,6 @@ function App() {
               </Routes>
             </main>
           </div>
-        // ) : (
-        //   <Routes>
-        //     <Route path="/sing-in" element={<SignInSide />} />
-        //   </Routes>
-        // )}
       </ThemeProvider>
     </ColorModeContext.Provider>
   );
