80084fcadc493e5fe96cb7c0e28bc051e59dcabe
DungPC <dungnt30101995@gmail.com>
2023-09-20 15:27:58 +0700

    push

--- server/index.js
+++ server/index.js
@@ -11,28 +11,28 @@ var corsOptions = {
 };
 const Role = db.role;
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Db");
-  initial();
-});
-const initial = () => {
-  Role.create({
-    id: 1,
-    name: "user",
-  });
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and Resync Db");
+//   initial();
+// });
+// const initial = () => {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
 
-  Role.create({
-    id: 2,
-    name: "moderator",
-  });
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
 
-  Role.create({
-    id: 3,
-    name: "admin",
-  });
-};
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(function (req, res, next) {
--- src/views/dashboard/index.jsx
+++ src/views/dashboard/index.jsx
@@ -1,6 +1,35 @@
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "../../data/mockChartData";
+import PieChart from "../../components/PieChart";
+Chart.register(CategoryScale);
+
 const Dashboard = () => {
-    return <div>Dashboard</div>;
-  };
-  
-  export default Dashboard;
-  
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year),
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        borderColor: "black",
+        borderWidth: 2,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#f3ba2f",
+          "#2a71d0",
+          "#66CC00",
+          "#9019AE"
+        ],
+      },
+    ],
+  });
+
+  return (
+    <div className="dashboard">
+      <PieChart chartData={chartData} />
+    </div>
+  );
+};
+
+export default Dashboard;
